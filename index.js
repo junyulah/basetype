b37c0cb1cd26e3307b98af4135dd378e8669e51b
loveKino <842914439@qq.com>
2016-08-05 19:09:15 +0800

    init

---
+++ index.js
@@ -0,0 +1,33 @@
+'use strict';
+
+let isArray = v => v && typeof v === 'object' && typeof v.length === 'number';
+
+let isString = v => typeof v === 'string';
+
+let isObject = v => v && typeof v === 'object';
+
+let isFunction = v => typeof v === 'function';
+
+let isNumber = v => typeof v === 'number';
+
+let isBool = v => typeof v === 'boolean';
+
+let isNode = (o) => {
+    return (
+        typeof Node === 'object' ? o instanceof Node :
+        o && typeof o === 'object' && typeof o.nodeType === 'number' && typeof o.nodeName === 'string'
+    );
+};
+
+let isPromise = v => v && typeof v === 'object' && typeof v.then === 'function' && typeof v.catch === 'function';
+
+module.exports = {
+    isArray,
+    isString,
+    isObject,
+    isFunction,
+    isNumber,
+    isBool,
+    isNode,
+    isPromise
+};
