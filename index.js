084df114d63cc4b516ad3b7bc86fdddcd200add9
loveKino <842914439@qq.com>
2016-08-07 00:24:08 +0800

    opt

--- index.js
+++ index.js
@@ -25,6 +25,106 @@ let isNode = (o) => {
 
 let isPromise = v => v && typeof v === 'object' && typeof v.then === 'function' && typeof v.catch === 'function';
 
+/**
+ * check type
+ *
+ * types = [typeFun]
+ */
+let funType = (fun, types = []) => {
+    if (!isFunction(fun)) {
+        throw new TypeError(`Expect function type arg[fun] in checkFunType, but got type ${typeof fun}, and value is ${fun}`);
+    }
+
+    if (!likeArray(types)) {
+        throw new TypeError(`Expect Array type arg[types] in checkFunType, but got type ${typeof types}, and value is ${types}`);
+    }
+
+    for (let i = 0; i < types.length; i++) {
+        let typeFun = types[i];
+        if (typeFun) {
+            if (!isFunction(typeFun)) {
+                throw new TypeError(`Expect function for typeFun in types ${i}. But got type ${typeof typeFun}, ${typeFun}.`);
+            }
+        }
+    }
+
+    return function() {
+        // check type
+        for (let i = 0; i < types.length; i++) {
+            let typeFun = types[i];
+            let arg = arguments[i];
+            if (typeFun && !typeFun(arg)) {
+                throw new TypeError(`Argument type error. Arguments order ${i}. Argument is ${arg}.`);
+            }
+        }
+        // result
+        return fun.apply(this, arguments);
+    };
+};
+
+let and = (...args) => {
+    if (!any(args, isFunction)) {
+        throw new TypeError('The argument of and must be function.');
+    }
+    return (v) => {
+        for (let i = 0; i < args.length; i++) {
+            let typeFun = args[i];
+            if (!typeFun(v)) {
+                return false;
+            }
+        }
+        return true;
+    };
+};
+
+let or = (...args) => {
+    if (!any(args, isFunction)) {
+        throw new TypeError('The argument of and must be function.');
+    }
+
+    return (v) => {
+        for (let i = 0; i < args.length; i++) {
+            let typeFun = args[i];
+            if (typeFun(v)) {
+                return true;
+            }
+        }
+        return false;
+    };
+};
+
+let any = (list, type) => {
+    if (!likeArray(list)) {
+        throw new TypeError(`Expect Array type, but got type ${typeof list}, and value is ${list}`);
+    }
+    if (!isFunction(type)) {
+        throw new TypeError(`Expect function type, but got type ${typeof type}, and value is ${type}`);
+    }
+
+    for (let i = 0; i < list.length; i++) {
+        if (!type(list[i])) {
+            return false;
+        }
+    }
+    return true;
+};
+
+let exist = (list, type) => {
+    if (!likeArray(list)) {
+        throw new TypeError(`Expect Array type, but got type ${typeof list}, and value is ${list}`);
+    }
+    if (!isFunction(type)) {
+        throw new TypeError(`Expect function type, but got type ${typeof type}, and value is ${type}`);
+    }
+
+    for (let i = 0; i < list.length; i++) {
+        if (type(list[i])) {
+            return true;
+        }
+    }
+    return false;
+};
+
 module.exports = {
     likeArray,
     isString,
@@ -33,5 +133,13 @@ module.exports = {
     isNumber,
     isBool,
     isNode,
-    isPromise
+    isPromise,
+
+    funType,
+
+    any,
+    exist,
+
+    and,
+    or
 };
